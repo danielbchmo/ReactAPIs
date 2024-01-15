4920108babe2f099944a629d6a69ba94bc68ecc6
danielbchmo <115262423+danielbchmo@users.noreply.github.com>
2024-01-15 11:57:55 -0600

    Adding loading state

--- apisreact/src/App.js
+++ apisreact/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useFetch } from './useFetch';
 
 function App() {
+  const { data, loading } = useFetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fetch - EndPoints</h1>
+      <div>
+        <ul>
+          {loading && <li>Loading...</li>}
+          {data?.map((user) => (
+            <li key={user.id}> {user.name} </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
