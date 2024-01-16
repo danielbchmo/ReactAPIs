a012c29b5e3d53802630c2373cd62144a2738442
danielbchmo <115262423+danielbchmo@users.noreply.github.com>
2024-01-16 17:12:36 -0600

    Adding and finishing with Apis pro

--- apisreact/src/App.js
+++ apisreact/src/App.js
@@ -1,4 +1,7 @@
-import './App.css';
+/*import './App.css';
+
+BEGINING METHOD
+
 import { useFetch } from './useFetch';
 
 function App() {
@@ -20,6 +23,32 @@ function App() {
        </ul>
      </div>
    );
+}
+
+export default App;*/
+
+//PROFESSIONAL METHOD
+import { fetchData } from "./fetchData";
+import { Suspense } from "react";
+
+const apiData = fetchData("https://jsonplaceholder.typicode.com/todos");
+
+function App(){ 
+  const data = apiData.read();
+
+  return (
+    <div>
+      <h1>API'S React</h1>
+      <Suspense>
+        <ul>
+          {data?.map((user) => (
+            <li key={user.id}> {user.name} </li>
+          ))}
+          
+        </ul>
+      </Suspense>
+    </div>
+  )
 }
 
 export default App;
