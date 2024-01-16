4d5731a945b6eeb25377cff4ad2ba842c0c39a26
danielbchmo <115262423+danielbchmo@users.noreply.github.com>
2024-01-16 15:58:35 -0600

    Adding cancel request

--- apisreact/src/App.js
+++ apisreact/src/App.js
@@ -2,23 +2,24 @@ import './App.css';
 import { useFetch } from './useFetch';
 
 function App() {
-  const { data, loading, error } = useFetch(
-    "https://jsonplaceholder.typicode.com/users"
+  const { data, loading, error, handleCancelRequest } = useFetch(
+    "https://jsonplaceholder.typicode.com/todos"
   );
 
   return (
     <div className="App">
-      <h1>Fetch - EndPoints</h1>
-      <div>
-        <ul>
-          {loading && <li>Loading...</li>}
-          {data?.map((user) => (
-            <li key={user.id}> {user.name} </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+      <h1>Fetch like a Pro</h1>
+
+       <button onClick={handleCancelRequest}>Cancel Request</button>
+       <ul className="card">
+         {error && <li>Error: {error}</li>}
+         {loading && <li>Loading...</li>}
+         {data?.map((item) => (
+           <li key={item.id}>{item.title}</li>
+         ))}
+       </ul>
+     </div>
+   );
 }
 
 export default App;
--- apisreact/src/useFetch.js
+++ apisreact/src/useFetch.js
@@ -4,16 +4,33 @@ export function useFetch(url) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [controller, setController] = useState(null);
 
   useEffect(() => {
-    fetch(url)
-    .then((response) => response.json())
-    .then((data) => setData(data))
-    .catch((error => setError(error)))
-    .finally(() => setLoading(false));
+    const abortController = new AbortController();
+    setController(abortController);
+
+    fetch(url, { signal: abortController.signal })
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        if (error.name === "AbortError") {
+          console.log("Cancelled request");
+        } else {
+          setError(error);
+        }
+      })
+      .finally(() => setLoading(false));
+
+    return () => abortController.abort();
   }, []);
 
-  
-  return { data, loading, error };
-}
+  const handleCancelRequest = () => {
+    if (controller) {
+      controller.abort();
+      setError("Cancelled Request");
+    }
+  };
 
+  return { data, loading, error, handleCancelRequest };
+}
